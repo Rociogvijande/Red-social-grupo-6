1f9b78233ac5a0ad1b28b5fd4cd6eed9e6c4edd7
romoma <romoma@hotmail.com>
2023-03-20 22:07:47 +0100

    ejercicio incremental 12

--- incremental11.js
+++ incremental11.js
@@ -7,15 +7,18 @@ var cursosRealizados;
 var anoNacimiento;
 
 // introduccion de datos usuario
+
 usuario = prompt('Introduzca su Nombre: ');
 apellido = prompt('Introduzca su Apellido: ');
 fechaNacimiento = prompt('Introduzca su Fecha Nacimineto: ');
 cursosRealizados = prompt('Introduzca el numeoro de cursos realizadoso: ');
 
 // Pasamos el formato estring de fechaNacimiento a formato tipo fecha con dd-mm-yyyy
+
 fechaNacimiento = new.Date(fechaNacimiento);
+
 //comprobamos si el año de fechaNacimiento es menor del año 2000
 
-if getFullYear(fechaNacimiento)< 2000 {
+if getFullYear(fechaNacimiento) < 2000 {
     alert('Estas seguro que eres un programador Junior?');
 }
